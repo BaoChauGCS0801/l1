3903fec71664880bbc7a06b955506527bb9b3757
BaoChauGCS0801 <71753891+BaoChauGCS0801@users.noreply.github.com>
2020-11-06 15:51:30 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://chau14:1234@cluster0.gyuow.mongodb.net/Shoplego?retryWrites=true&w=majority',
+        dbname : "Shoplego",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://chau14:1234@cluster0.gyuow.mongodb.net/Shoplego?retryWrites=true&w=majority',
+        dbname : "Shoplego",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
